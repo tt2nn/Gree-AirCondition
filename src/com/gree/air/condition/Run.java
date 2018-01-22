17c1e4f917d2e7a4f5b76a3f2fe6625e23eaa8c3
lihaotian <lihaotian@mirror-networks.com>
2018-01-22 18:57:17 +0800

    设置APN

--- src/com/gree/air/condition/Run.java
+++ src/com/gree/air/condition/Run.java
@@ -4,6 +4,7 @@ import com.gree.air.condition.center.DataCenter;
 import com.gree.air.condition.center.Timer;
 import com.gree.air.condition.configure.Configure;
 import com.gree.air.condition.configure.DeviceConfigure;
+import com.gree.air.condition.constant.Constant;
 import com.gree.air.condition.entity.Apn;
 import com.gree.air.condition.sms.SmsServer;
 import com.gree.air.condition.uart.UartServer;
@@ -26,6 +27,9 @@ public class Run {
 
 		Timer.startTimer();
 
+		DeviceConfigure.deviceInit();
+		DeviceConfigure.setApn(new Apn());
+
 		try {
 
 			while (DeviceConfigure.deviceInit()) {
@@ -35,9 +39,18 @@ public class Run {
 
 			DeviceConfigure.deviceInfo();
 
-			DeviceConfigure.setApn(new Apn());
+			Constant.System_Time = 60000;
+
+			Constant.Gprs_Mac[0] = (byte) Integer.parseInt(Constant.device.getImei().substring(1, 3));
+			Constant.Gprs_Mac[1] = (byte) Integer.parseInt(Constant.device.getImei().substring(3, 5));
+			Constant.Gprs_Mac[2] = (byte) Integer.parseInt(Constant.device.getImei().substring(5, 7));
+			Constant.Gprs_Mac[3] = (byte) Integer.parseInt(Constant.device.getImei().substring(7, 9));
+			Constant.Gprs_Mac[4] = (byte) Integer.parseInt(Constant.device.getImei().substring(9, 11));
+			Constant.Gprs_Mac[5] = (byte) Integer.parseInt(Constant.device.getImei().substring(11, 13));
+			Constant.Gprs_Mac[6] = (byte) Integer.parseInt(Constant.device.getImei().substring(13, 15));
 
 			SmsServer.startServer();
+
 			DataCenter.startUploadData();
 
 		} catch (InterruptedException e) {
