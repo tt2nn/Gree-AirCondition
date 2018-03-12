8327c354d4f1274c3a9c461da772e183da1a26d8
lihaotian <lihaotian@mirror-networks.com>
2018-03-12 10:36:55 +0800

    v0.2

--- src/com/gree/air/condition/constant/Constant.java
+++ src/com/gree/air/condition/constant/Constant.java
@@ -8,9 +8,9 @@ package com.gree.air.condition.constant;
  */
 public class Constant {
 
-	public static final String APP_VERSION = "V0.1";
+	public static final String APP_VERSION = "V0.2";
 	public static final byte APP_VERSION_FIRST = (byte) 0x00;
-	public static final byte APP_VERSKON_SECOND = (byte) 0x01;
+	public static final byte APP_VERSKON_SECOND = (byte) 0x02;
 
 	public static final int BAUD_RATE = 9600;
 	public static final byte GPRS_MODEL = (byte) 0x02;
